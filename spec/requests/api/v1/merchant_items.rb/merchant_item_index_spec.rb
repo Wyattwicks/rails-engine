3a27f609f5282691141176bd882a476270dd9e52
Wyattwicks <wyattwicks@gmail.com>
2021-05-25 18:01:12 -0600

    Add working merchant items

--- spec/requests/api/v1/merchant_items.rb/merchant_item_index_spec.rb
+++ spec/requests/api/v1/merchant_items.rb/merchant_item_index_spec.rb
@@ -4,10 +4,17 @@ RSpec.describe  'MerchantItemController', type: :request do
   describe "merchant item index" do 
     it 'fetches all items that belong to a merchant by ID' do
       merchant = create(:merchant)
-      create_list(:item, 10, :merchant_id: merchant.id)
+      extra_merchant = create(:merchant)
+      other_item = create(:item, merchant_id: extra_merchant.id)
+      create_list(:item, 10, merchant_id: merchant.id)
+
 
       get "/api/v1/merchants/#{merchant.id}/items"
       
+      items = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+      expect(items[:data].count).to eq(10)
     end
   end
 end
