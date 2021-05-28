eea0de43bc6f1fa83a9ce1cf5f08e565bdafa9ee
Wyattwicks <wyattwicks@gmail.com>
2021-05-27 20:25:42 -0600

    Add full solution for merchant total revenue

--- app/controllers/api/v1/revenue/merchants_controller.rb
+++ app/controllers/api/v1/revenue/merchants_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::Revenue::MerchantsController < ApplicationController
   def total_revenue
-    if params[:quantity].to_i > 0 || params[:quantity].present?
+    if params[:quantity].to_i > 0 && params[:quantity].present?
       @merchants = Merchant.total_revenue_ranked(params[:quantity])
       render json: MerchantNameRevenueSerializer.new(@merchants)
-      
-
-
+    else
+      render json: { error: "error"}, status: 400
+    end
   end
   
   def single_merchant_revenue
